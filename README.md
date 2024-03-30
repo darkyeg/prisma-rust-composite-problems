55c66f61e969fdaab09baf0bfe0909c81e58c7b1
Youssef Khalil <38569107+darkyeg@users.noreply.github.com>
2024-03-30 15:37:52 +0200

    nullable

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Prisma Client Rust Problems
+### The Problem:
 
-## Branches
+The provided branch encountering two issues related to handling nullable fields (`Language?`) in the generated code.
 
-1. [Unique constraints with composite type fields](./tree/unique-constraint)
+#### Error 1:
+
+When attempting to match products with a defined name, the code encounters a parsing/validation error. It fails to match the input value to any allowed input type for the `name` field, resulting in a type mismatch error.
+
+#### Error 2:
+
+The generated code lacks proper handling for equality checks (`equal(Null)`, `is(Null)`, `is_not(Null)`) for nullable fields. This omission is crucial, especially when dealing with nullable fields like `name`.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use db::LazyClient;
+use db::{product, LazyClient};
 
 #[allow(non_upper_case_globals)]
 static client: LazyClient = LazyClient::new();
@@ -14,5 +14,18 @@ async fn main() {
   // The problem
   // .................................
 
-  println!("Hello, world!");
+  // 1 - Error
+  // Unable to match input value to any allowed input type for the field. Parse errors:
+  // [Query parsing/validation error at `Query.findFirstProduct.where.ProductWhereInput.name`: Value types mismatch. Have: Boolean(true), want: Object(LanguageNullableCompositeFilter), Query parsing/validation error at `Query.findFirstProduct.where.ProductWhereInput.name`: Value types mismatch. Have: Boolean(true), want: Object(LanguageObjectEqualityInput), Query parsing/validation error at `Query.findFirstProduct.where.ProductWhereInput.name`: Value types mismatch. Have: Boolean(true), want: Null]"),
+  // "query_position": String("Query.findFirstProduct.where.ProductWhereInput.name
+  let result = client
+    .product()
+    .find_first(vec![product::name::is_set()])
+    .exec()
+    .await;
+
+  println!("{:#?}", result);
+
+  // 2 - Missing equal/is/is_not(Null) in generated code
+  // it is so important when use nullable fields
 }
